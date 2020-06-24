278812e4f73aee5ac80dcdaf53191ac3c4349884
Panda <panda@sumologic.com>
2020-06-23 17:46:31 -0700

    add trim to all the fields

--- windows_fer.tf
+++ windows_fer.tf
@@ -28,10 +28,15 @@ resource "sumologic_field_extraction_rule" "WindowsEvent4624" {
   EOT
   parse_expression = <<-EOT
   parse "Logon Type:*\r\n" as EventData_LogonType
+  | trim(EventData_LogonType)
   | parse "New Logon:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "Process Information:\r\n\tProcess ID:*\r\n\tProcess Name:*\r\n" as EventData_ProcessId,EventData_ProcessName
+  | trim(EventData_ProcessId) | trim(EventData_ProcessName)
   | parse "Network Information:\r\n\tWorkstation Name:*\r\n\tSource Network Address:*\r\n\tSource Port:*\r\n" as EventData_WorkstationName,EventData_IpAddress,EventData_IpPort
+  | trim(EventData_WorkstationName) | trim(EventData_IpAddress) | trim(EventData_IpPort)
   | parse "Detailed Authentication Information:\r\n\tLogon Process:*\r\n" as EventData_LogonProcessName
+  | trim(EventData_LogonProcessName)
   | fields - _1
   EOT
   enabled = false
@@ -44,12 +49,19 @@ resource "sumologic_field_extraction_rule" "WindowsEvent4625" {
   EOT
   parse_expression = <<-EOT
   parse "Logon Type:*\r\n" as EventData_LogonType
+  | trim(EventData_LogonType)
   | parse "Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,_2,_3,EventData_LogonId
+  | trim(EventData_LogonId)
   | parse "Account For Which Logon Failed:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n" as _4,EventData_SubjectUserName,EventData_SubjectDomainName
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName)
   | parse "Failure Information:\r\n\tFailure Reason:*\r\n\tStatus:*\r\n\tSub Status:*\r\n" as EventData_FailureReason,EventData_Status,EventData_SubStatus
-  | parse "Process Information:\r\n\tCaler Process ID:*\r\n\tCaller Process Name:*\r\n" as EventData_ProcessId,EventData_ProcessName
+  | trim(EventData_FailureReason) | trim(EventData_Status) | trim(EventData_SubStatus)
+  | parse "Process Information:\r\n\tCaller Process ID:*\r\n\tCaller Process Name:*\r\n" as EventData_ProcessId,EventData_ProcessName
+  | trim(EventData_ProcessId) | trim(EventData_ProcessName)
   | parse "Network Information:\r\n\tWorkstation Name:*\r\n\tSource Network Address:*\r\n\tSource Port:*\r\n" as EventData_WorkstationName,EventData_IpAddress,EventData_IpPort
+  | trim(EventData_WorkstationName) | trim(EventData_IpAddress) | trim(EventData_IpPort)
   | parse "Detailed Authentication Information:\r\n\tLogon Process:*\r\n" as EventData_LogonProcessName
+  | trim(EventData_LogonProcessName)
   | fields - _1,_2,_3,_4
   EOT
   enabled = false
@@ -62,7 +74,9 @@ resource "sumologic_field_extraction_rule" "WindowsEvent4627" {
   EOT
   parse_expression = <<-EOT
   parse "Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "New Logon:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n" as _2,EventData_TargetUserName,EventData_TargetDomainName
+  | trim(EventData_TargetUserName) | trim(EventData_TargetDomainName)
   | fields - _1,_2
   EOT
   enabled = false
@@ -75,9 +89,13 @@ resource "sumologic_field_extraction_rule" "WindowsEvent4648" {
   EOT
   parse_expression = <<-EOT
   parse "Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "Account Whose Credentials Were Used:\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n" as EventData_TargetUserName,EventData_TargetDomainName
+  | trim(EventData_TargetUserName) | trim(EventData_TargetDomainName)
   | parse "Process Information:\r\n\tProcess ID:*\r\n\tProcess Name:*\r\n" as EventData_ProcessId,EventData_ProcessName
+  | trim(EventData_ProcessId) | trim(EventData_ProcessName)
   | parse "Network Information:\r\n\tNetwork Address:*\r\n\tPort:*\r\n" as EventData_IpAddress,EventData_IpPort
+  | trim(EventData_IpAddress) | trim(EventData_IpPort)
   | fields - _1
   EOT
   enabled = false
@@ -90,8 +108,11 @@ resource "sumologic_field_extraction_rule" "WindowsEvent4656" {
   EOT
   parse_expression = <<-EOT
   parse "Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "Object Name:*\r\n" as EventData_ObjectDN
+  | trim(EventData_ObjectDN)
   | parse "Process Information:\r\n\tProcess ID:*\r\n\tProcess Name:*\r\n" as EventData_ProcessId,EventData_ProcessName
+  | trim(EventData_ProcessId) | trim(EventData_ProcessName)
   | fields - _1
   EOT
   enabled = false
@@ -104,7 +125,9 @@ resource "sumologic_field_extraction_rule" "WindowsEvent4662" {
   EOT
   parse_expression = <<-EOT
   parse "Subject :\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "Object Name:*\r\n" as EventData_ObjectDN
+  | trim(EventData_ObjectDN)
   | fields - _1
   EOT
   enabled = false
@@ -117,8 +140,11 @@ resource "sumologic_field_extraction_rule" "WindowsEvent4663" {
   EOT
   parse_expression = <<-EOT
   parse "Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "Object Name:*\r\n" as EventData_ObjectDN
+  | trim(EventData_ObjectDN)
   | parse "Process Information:\r\n\tProcess ID:*\r\n\tProcess Name:*\r\n" as EventData_ProcessId,EventData_ProcessName
+  | trim(EventData_ProcessId) | trim(EventData_ProcessName)
   | fields - _1
   EOT
   enabled = false
@@ -131,8 +157,11 @@ resource "sumologic_field_extraction_rule" "WindowsEvent4670" {
   EOT
   parse_expression = <<-EOT
   parse "Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "Object Name:*\r\n" as EventData_ObjectDN
+  | trim(EventData_ObjectDN)
   | parse "Process:\r\n\tProcess ID:*\r\n\tProcess Name:*\r\n" as EventData_ProcessId,EventData_ProcessName
+  | trim(EventData_ProcessId) | trim(EventData_ProcessName)
   | fields - _1
   EOT
   enabled = false
@@ -145,7 +174,9 @@ resource "sumologic_field_extraction_rule" "WindowsEvent4673" {
   EOT
   parse_expression = <<-EOT
   parse "Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "Process:\r\n\tProcess ID:*\r\n\tProcess Name:*\r\n" as EventData_ProcessId,EventData_ProcessName
+  | trim(EventData_ProcessId) | trim(EventData_ProcessName)
   | fields - _1
   EOT
   enabled = false
@@ -158,8 +189,11 @@ resource "sumologic_field_extraction_rule" "WindowsEvent4674" {
   EOT
   parse_expression = <<-EOT
   parse "Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "Object Name:*\r\n" as EventData_ObjectDN
+  | trim(EventData_ObjectDN)
   | parse "Process Information:\r\n\tProcess ID:*\r\n\tProcess Name:*\r\n" as EventData_ProcessId,EventData_ProcessName
+  | trim(EventData_ProcessId) | trim(EventData_ProcessName)
   | fields - _1
   EOT
   enabled = false
@@ -172,8 +206,11 @@ resource "sumologic_field_extraction_rule" "WindowsEvent46788" {
   EOT
   parse_expression = <<-EOT
   parse "Creator Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "Target Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n" as _2,EventData_TargetUserName,EventData_TargetDomainName
+  | trim(EventData_TargetUserName) | trim(EventData_TargetDomainName)
   | parse "Process Information:\r\n\tNew Process ID:*\r\n\tNew Process Name:*\r\n\tToken Elevation Type:*\r\n\tMandatory Label:*\r\n\tCreator Process ID:*\r\n\tCreator Process Name:*\r\n\tProcess Command Line:*\r\n" as EventData_ProcessId,EventData_ProcessName,_3,_4,_5,EventData_ParentProcess,EventData_CommandLine
+  | trim(EventData_ProcessId) | trim(EventData_ProcessName) | trim(EventData_ParentProcess) | trim(EventData_CommandLine)
   | fields - _1,_2,_3,_4,_5
   EOT
   enabled = false
@@ -186,8 +223,11 @@ resource "sumologic_field_extraction_rule" "WindowsEvent4691" {
   EOT
   parse_expression = <<-EOT
   parse "Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "Object Name:*\r\n" as EventData_ObjectDN
+  | trim(EventData_ObjectDN)
   | parse "Process Information:\r\n\tProcess ID:*\r\n" as EventData_ProcessId
+  | trim(EventData_ProcessId)
   | fields - _1
   EOT
   enabled = false
@@ -200,8 +240,11 @@ resource "sumologic_field_extraction_rule" "WindowsEvent4696" {
   EOT
   parse_expression = <<-EOT
   parse "Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "Process Information:\r\n\tProcess ID:*\r\n\tProcess Name:*\r\n" as EventData_ProcessId,EventData_ProcessName
+  | trim(EventData_ProcessId) | trim(EventData_ProcessName)
   | parse "New Token Information:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n" as _2,EventData_TargetUserName,EventData_TargetDomainName
+  | trim(EventData_TargetUserName) | trim(EventData_TargetDomainName)
   | fields - _1,_2
   EOT
   enabled = false
@@ -214,7 +257,9 @@ resource "sumologic_field_extraction_rule" "WindowsEvent4697" {
   EOT
   parse_expression = <<-EOT
   parse "Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "Service Information:\r\n\tService Name:*\r\n\tService File Name:*\r\n" as EventData_ServiceName,EventData_ServiceFileName
+  | trim(EventData_ServiceName) | trim(EventData_ServiceFileName)
   | fields - _1
   EOT
   enabled = false
@@ -227,7 +272,9 @@ resource "sumologic_field_extraction_rule" "WindowsEventScheduledTasks" {
   EOT
   parse_expression = <<-EOT
   parse "Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "Task Information:\r\n\tTask Name:*\r\n" as EventData_TaskName
+  | trim(EventData_TaskName)
   | fields - _1
   EOT
   enabled = false
@@ -240,7 +287,9 @@ resource "sumologic_field_extraction_rule" "WindowsEventAccounts" {
   EOT
   parse_expression = <<-EOT
   parse "Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "Target Account:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n" as _2,EventData_TargetUserName,EventData_TargetDomainName
+  | trim(EventData_TargetUserName) | trim(EventData_TargetDomainName)
   | fields - _1,_2
   EOT
   enabled = false
@@ -253,7 +302,9 @@ resource "sumologic_field_extraction_rule" "WindowsEventGroupCreation" {
   EOT
   parse_expression = <<-EOT
   parse "Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "New Group:\r\n\tSecurity ID:*\r\n\tGroup Name:*\r\n" as _2,EventData_GroupName
+  | trim(EventData_GroupName)
   | fields - _1,_2
   EOT
   enabled = false
@@ -266,7 +317,9 @@ resource "sumologic_field_extraction_rule" "WindowsEventGroupMgmt" {
   EOT
   parse_expression = <<-EOT
   parse "Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "Group:\r\n\tSecurity ID:*\r\n\tGroup Name:*\r\n" as _2,EventData_GroupName
+  | trim(EventData_GroupName)
   | fields - _1,_2
   EOT
   enabled = false
@@ -279,7 +332,9 @@ resource "sumologic_field_extraction_rule" "WindowsEvent4740" {
   EOT
   parse_expression = <<-EOT
   parse "Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "Additional Information:\r\n\tCaller Computer Name:*\"" as EventData_CallerComputer
+  | trim(EventData_CallerComputer)
   | fields - _1
   EOT
   enabled = false
@@ -292,8 +347,11 @@ resource "sumologic_field_extraction_rule" "WindowsEventKerberos" {
   EOT
   parse_expression = <<-EOT
   parse "Account Information:\r\n\tAccount Name:*\r\n" as EventData_SubjectUserName
+  | trim(EventData_SubjectUserName)
   | parse "Network Information:\r\n\tClient Address:*::ffff:*\r\n\tClient Port:*\r\n" as _1,EventData_IpAddress,EventData_IpPort
+  | trim(EventData_IpAddress) | trim(EventData_IpPort)
   | parse "Ticket Encryption Type:*\r\n" as EventData_TicketEncryption
+  | trim(EventData_TicketEncryption)
   | fields - _1
   EOT
   enabled = false
@@ -306,8 +364,11 @@ resource "sumologic_field_extraction_rule" "WindowsEvent4771" {
   EOT
   parse_expression = <<-EOT
   parse "Account Information:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n" as _1,EventData_SubjectUserName
+  | trim(EventData_SubjectUserName)
   | parse "Network Information:\r\n\tClient Address:*::ffff:*\r\n\tClient Port:*\r\n" as _2,EventData_IpAddress,EventData_IpPort
+  | trim(EventData_IpAddress) | trim(EventData_IpPort)
   | parse "Failure Code:*\r\n" as EventData_FailureCode
+  | trim(EventData_FailureCode)
   | fields - _1,_2
   EOT
   enabled = false
@@ -320,7 +381,9 @@ resource "sumologic_field_extraction_rule" "WindowsEvent4776" {
   EOT
   parse_expression = <<-EOT
   parse "Source Workstation:*\r\n" as EventData_WorkstationName
+  | trim(EventData_WorkstationName)
   | parse "Error Code:*\"" as EventData_FailureCode
+  | trim(EventData_FailureCode)
   EOT
   enabled = false
 }
@@ -332,8 +395,11 @@ resource "sumologic_field_extraction_rule" "WindowsEvent4799" {
   EOT
   parse_expression = <<-EOT
   parse "Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "Group:\r\n\tSecurity ID:*\r\n\tGroup Name:*\r\n" as _2,EventData_GroupName
+  | trim(EventData_GroupName)
   | parse "Process Information:\r\n\tProcess ID:*\r\n\tProcess Name:*\"" as EventData_ProcessId,EventData_ProcessName
+  | trim(EventData_ProcessId) | trim(EventData_ProcessName)
   | fields - _1,_2
   EOT
   enabled = false
@@ -346,8 +412,11 @@ resource "sumologic_field_extraction_rule" "WindowsEvent49074911" {
   EOT
   parse_expression = <<-EOT
   parse "Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "Process Information:\r\n\tProcess ID:*\r\n\tProcess Name:*\r\n" as EventData_ProcessId,EventData_ProcessName
+  | trim(EventData_ProcessId) | trim(EventData_ProcessName)
   | parse "Object Name:*\r\n" as EventData_ObjectDN
+  | trim(EventData_ObjectDN)
   | fields - _1
   EOT
   enabled = false
@@ -360,8 +429,11 @@ resource "sumologic_field_extraction_rule" "WindowsEvent5140" {
   EOT
   parse_expression = <<-EOT
   parse "Subject:\r\n\tSecurity ID:*\r\n\tAccount Name:*\r\n\tAccount Domain:*\r\n\tLogon ID:*\r\n" as _1,EventData_SubjectUserName,EventData_SubjectDomainName,EventData_LogonId
+  | trim(EventData_SubjectUserName) | trim(EventData_SubjectDomainName) | trim(EventData_LogonId)
   | parse "Network Information:*\r\n\tObject Type:*\r\n\tSource Address:*\r\n\tSource Port:*\r\n" as _2,_3,EventData_IpAddress,EventData_IpPort
+  | trim(EventData_IpAddress) | trim(EventData_IpPort)
   | parse "Share Name:*\r\n" as EventData_ShareName
+  | trim(EventData_ShareName)
   | fields - _1,_2,_3
   EOT
   enabled = false
